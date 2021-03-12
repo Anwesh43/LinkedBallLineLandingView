0be61aad2e621a88a22f8fd7342208f8588c4808
Anwesh43 <anweshthecool0@gmail.com>
2021-03-13 02:39:24 +0530

    adding Canvas extension methods

--- balllinelandingview/src/main/java/com/example/balllinelandingview/BallLineLandingView.kt
+++ balllinelandingview/src/main/java/com/example/balllinelandingview/BallLineLandingView.kt
@@ -30,3 +30,34 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBallLineLanding(scale : Float, w : Float, h : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val r : Float = Math.min(w, h)  / rFactor
+    val lineSize : Float = Math.min(w, h) / lineFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(w / 2, (h - r - lineSize) * sf3)
+    drawCircle(0f, r, r * sf1, paint)
+    for (j in 0..1) {
+        save()
+        rotate(45f * (1f - 2 * j) * sf4)
+        drawLine(0f, r, 0f, r + lineSize * sf2, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBLLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBallLineLanding(scale, w, h, paint)
+}
